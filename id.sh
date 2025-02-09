55b5dc1a2e1b13324cd9c4a71fcbcfa502da4563
Thon Ly <thonly@heartbank.ceo>
2025-02-09 07:33:43 -0800

    add dict

---
+++ id.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Read the JSON file and process each object
+jq -c '.[]' Dictionary.json | while read -r item; do
+    id=$(echo "$item" | jq -r '.id')  # Extract the ID
+    echo "$item" > "id/$id.json"       # Save as id.json inside the id folder
+    echo "$id created"
+done
---
+++ word.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Read the JSON file and process each object
+jq -c '.[]' Dictionary.json | while read -r item; do
+    word=$(echo "$item" | jq -r '.word')  # Extract the word
+    echo "$item" > "word/$word.json"      # Save as word.json inside the name folder
+    echo "$word created"
+done
